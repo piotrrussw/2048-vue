0e97e97ca4dabb0f9306249c483e61dab65ccbe1
piotrrussw <piotr.wadycki@gmail.com>
2018-10-13 15:55:26 +0200

    randomCell method refactor

--- js/main.js
+++ js/main.js
@@ -34,13 +34,12 @@ new Vue({
 
         addTile(tile) {
             const tileIndex = this.tiles.findIndex(item => item.x === tile.x && item.y === tile.y) || -1;
-            return this.tiles[tileIndex] = tile;
+            return this.tiles.splice(tileIndex, 1, tile);
         },
 
         addRandomTile() {
             const value = Math.random() < 0.8 ? 2 : 4;
             const position = this.randomCell() || [];
-
             this.addTile({
                 x: position.x,
                 y: position.y,
@@ -51,15 +50,14 @@ new Vue({
 
         randomCell() {
             const filteredTiles = this.tiles.filter(item => item.value === 0) || [];
-            const availableX = filteredTiles.map(item => item.x);
-            const availableY = filteredTiles.map(item => item.y);
+            const availablePos = filteredTiles.length ? filteredTiles[Math.floor(Math.random()*filteredTiles.length)] : -1;
 
-            const posX = availableX.length ? availableX[Math.floor(Math.random()*availableX.length)] : -1;
-            const posY = availableY.length ? availableY[Math.floor(Math.random()*availableY.length)] : -1;
-
-            return posY !== -1 && posX !== -1  ? {
-                x: posX,
-                y: posY
+            const x = availablePos.x;
+            const y = availablePos.y;
+            console.log(x, y);
+            return availablePos !== -1 ? {
+                x,
+                y
             } : this.gameOver();
         },
 
